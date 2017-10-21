f940921d6587dd9c832916dfab911218d831eafa
Unknown <emilsergiev@abv.bg>
2017-10-21 05:16:15 +0300

    added owner and dates fields

--- src/app/models/request.ts
+++ src/app/models/request.ts
@@ -1,7 +1,9 @@
 export class Request {
     constructor(
+    	public owner: string,
         public type: string,
         public days: string,
+        public dates: string,
         public submit: string,
         public status: string
     ) {}
