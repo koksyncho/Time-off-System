1c5be234826cb4bb158c1b7ce4b71e21f06e07e0
Unknown <emilsergiev@abv.bg>
2017-10-22 20:14:08 +0300

    added the missing id fields

--- src/app/components/requests/requests.component.ts
+++ src/app/components/requests/requests.component.ts
@@ -19,13 +19,13 @@ export class RequestsComponent implements OnInit
     this.admin = true; //only for testing... *to be removed*
     this.pto = 15; //only for testing... *to be removed*
     this.requests = [
-      {"owner":"Teo", "type":"PTO", "days":"1", "dates":"5 September 2016", "submit":"2016-08-10 18:20:31", "status":"approved"},
-      {"owner":"Tito", "type":"PTO", "days":"8", "dates":"29-31 August, 1-2, 7-9 September 2016", "submit":"2016-06-09 18:32:48", "status":"approved"},
-      {"owner":"Lili", "type":"PTO", "days":"3", "dates":"27-29 June 2016", "submit":"2016-06-09 18:29:54", "status":"approved"},
-      {"owner":"...", "type":"...", "days":"...", "dates":"...", "submit":"...", "status":"..."},
-      {"owner":"...", "type":"...", "days":"...", "dates":"...", "submit":"...", "status":"..."},
-      {"owner":"...", "type":"...", "days":"...", "dates":"...", "submit":"...", "status":"..."},
-      {"owner":"...", "type":"...", "days":"...", "dates":"...", "submit":"...", "status":"..."}
+      {"id":"1", "owner":"Teo", "type":"PTO", "days":"1", "dates":"5 September 2016", "submit":"2016-08-10 18:20:31", "status":"approved"},
+      {"id":"2", "owner":"Tito", "type":"PTO", "days":"8", "dates":"29-31 August, 1-2, 7-9 September 2016", "submit":"2016-06-09 18:32:48", "status":"approved"},
+      {"id":"3", "owner":"Lili", "type":"PTO", "days":"3", "dates":"27-29 June 2016", "submit":"2016-06-09 18:29:54", "status":"approved"},
+      {"id":"4", "owner":"...", "type":"...", "days":"...", "dates":"...", "submit":"...", "status":"..."},
+      {"id":"5", "owner":"...", "type":"...", "days":"...", "dates":"...", "submit":"...", "status":"..."},
+      {"id":"6", "owner":"...", "type":"...", "days":"...", "dates":"...", "submit":"...", "status":"..."},
+      {"id":"7", "owner":"...", "type":"...", "days":"...", "dates":"...", "submit":"...", "status":"..."}
     ]; //this data will be coming from the server... this here is only for testing *to be removed*
   }
 
