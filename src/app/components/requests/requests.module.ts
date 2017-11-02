8cb222ec1d5e5dec18fe88e73fbbaf3a5f8920f1
ivo0126 <wibo0126@gmail.com>
2017-11-02 18:39:25 +0300

    Rough implementation of the angular-calendar

--- src/app/components/requests/requests.module.ts
+++ src/app/components/requests/requests.module.ts
@@ -5,14 +5,28 @@ import { UserService } from '../../services/user.service';
 
 import { RequestsComponent } from './requests.component';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'angular-calendar';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoUtilsModule } from '../demo-utils/module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     RequestsComponent
   ],
   imports: [
     BrowserModule,
-    RequestsRoutes
+    RequestsRoutes,
+    CommonModule,
+    FormsModule,
+    NgbModalModule.forRoot(),
+    CalendarModule.forRoot(),
+    DemoUtilsModule,
+    BrowserAnimationsModule
   ],
   providers: [UserService],
+  exports: [RequestsComponent]
 })
 export class RequestsModule {}
