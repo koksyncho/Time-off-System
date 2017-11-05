97e7366cf20a91ddf5e7433e6359b490cad532b1
koksyncho <hrokishmokibarbaroki@gmail.com>
2017-11-05 10:11:14 +0200

    Small user.ts changes
    Added some fields to the module and experimented with the local storage. Nothing substantial.

--- src/app/components/user-page/user-page.component.ts
+++ src/app/components/user-page/user-page.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 
+import { Router } from '@angular/router';
+
+import { Request } from '../../models/request';
+import { User } from '../../models/user';
+
 @Component({
   selector: 'app-user-page',
   templateUrl: '../../templates/user-page.component.html',
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent implements OnInit {
-
-  constructor(private userService: UserService) { }
+    users:User[];
+  	constructor(private userService: UserService) { 
+	  	this.users = [
+	      {"id": "1", "username": "Name", "email": "name@email.com", "password": "namepass"},
+	      {"id": "2", "username": "Name2", "email": "name2@email.com", "password": "name2pass"},
+	      {"id": "3", "username": "Name3", "email": "name3@email.com", "password": "name3pass"}
+	    ]; //this data will be coming from the server... this here is only for testing *to be removed*
+  	}
 
   ngOnInit() {
   }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,5 +1,6 @@
 export class User {
     constructor(
+    	public id: string,
         public username: string,
         public email: string,
         public password: string
