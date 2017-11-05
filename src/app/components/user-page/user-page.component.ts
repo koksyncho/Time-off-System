306b4e871e95b8aee2125cd6b889794f39344fa2
koksyncho <hrokishmokibarbaroki@gmail.com>
2017-11-05 10:44:16 +0200

    Fixed login errors and added the pdf notes back

--- src/app/components/user-page/user-page.component.ts
+++ src/app/components/user-page/user-page.component.ts
@@ -15,9 +15,9 @@ export class UserPageComponent implements OnInit {
     users:User[];
   	constructor(private userService: UserService) { 
 	  	this.users = [
-	      {"id": "1", "username": "Name", "email": "name@email.com", "password": "namepass"},
-	      {"id": "2", "username": "Name2", "email": "name2@email.com", "password": "name2pass"},
-	      {"id": "3", "username": "Name3", "email": "name3@email.com", "password": "name3pass"}
+	      {"username": "Name", "email": "name@email.com", "password": "namepass"},
+	      {"username": "Name2", "email": "name2@email.com", "password": "name2pass"},
+	      {"username": "Name3", "email": "name3@email.com", "password": "name3pass"}
 	    ]; //this data will be coming from the server... this here is only for testing *to be removed*
   	}
 
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,6 +1,5 @@
 export class User {
     constructor(
-    	public id: string,
         public username: string,
         public email: string,
         public password: string
